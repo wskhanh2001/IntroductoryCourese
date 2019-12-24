d2f20711c3933ebffcb01833b69d446490871afe
wskhanh2001 <57005233+wskhanh2001@users.noreply.github.com>
2019-12-24 18:39:34 +0800

    Update views.py

--- wsk/background_django/request_post/posts/views.py
+++ wsk/background_django/request_post/posts/views.py
@@ -5,14 +5,14 @@ import cv2 as cv
 import numpy as np
 # import tensorflow as tf
 import base64
-# from keras.models import load_model
+from tensorflow.keras.models import load_model
 # import keras
 # keras.backend.clear_session()
 
 
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# model = load_model('keras_cifar10_trained_model_1_2.h5')
+model = load_model('keras_cifar10_trained_model_1_2.h5')
 
 #classes_emotion = ['angry','disgust', 'fear','happy','sad', 'surprised', 'normal']
 
@@ -22,7 +22,7 @@ def response(request):
     if request.method == 'POST':  # 当提交表单时
         # 判断是否传参
         if request.POST:
-            z = []
+            z = [0,0,0,0]
             emotion = 0
             img_str = request.POST.get('recognize_img', 0)
             img_decode_ = img_str.encode('ascii')  # ascii编码
@@ -32,15 +32,15 @@ def response(request):
             gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
             img = cv.resize(gray, (48, 48))
             img = np.array(img).reshape((1,48, 48,1))
-            # classes = model.predict(img,batch_size = 128)
-            # emotion = np.argmax(classes)
+            classes = model.predict(img,batch_size = 128)
+            emotion = np.argmax(classes)
             faces = face_cascade.detectMultiScale(gray)
             for (x, y, w, h) in faces:
                     z = [x, y, w, h]
             print(z)
 
             if img_str:
-                data={'x':float(z[0]),'y':float(z[1]),'w':float(z[2]),'h':float(z[3])}
+                data={'x':float(z[0]),'y':float(z[1]),'w':float(z[2]),'h':float(z[3]),'emotion':float(emotion)}
                 son_mod = json.dumps(data)
                 print(data)
                 return HttpResponse(son_mod)
